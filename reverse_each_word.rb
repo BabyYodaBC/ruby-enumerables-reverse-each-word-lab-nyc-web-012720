2a08cbfe6a1eb64f92965bb6d2c5fabd8cff4f16
ricky wahba <rjwahba@gmail.com>
2020-01-16 00:49:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
   string = sentence.split(" ")
-  reverse = string.collect do |reverse_string| 
-    reverse_string = reverse_string.reverse
-  end
+  reverse = string.collect {|reverse_string| reverse_string.reverse}
   return reverse.join(' ')
 end
